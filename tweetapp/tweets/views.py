5393371e717fcc21516b04ecf6ba9c29556c5020
kaknjoh <kaknjo.harun2019@gmail.com>
2020-07-15 22:46:58 +0200

    Javascript for displaying tweets and liek button

--- tweetapp/tweets/views.py
+++ tweetapp/tweets/views.py
@@ -1,6 +1,8 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
+import random
 # Create your views here.
 
 
@@ -10,8 +12,10 @@ def home_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content,
+                    "likes": random.randint(0, 1305451)} for x in qs]
     data = {
+        "isUser": False,
         "response": tweets_list
     }
     return JsonResponse(data)
