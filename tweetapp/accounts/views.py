f8862d00ea0fc880326cbc3360efd2238f69a54a
kaknjoh <kaknjo.harun2019@gmail.com>
2020-07-26 13:33:23 +0200

    Signals to create profiles

--- tweetapp/accounts/views.py
+++ tweetapp/accounts/views.py
@@ -32,13 +32,13 @@ def logout_view(request, *args, **kwargs):
 
 
 def registration_view(request, *args, **kwargs):
-    form = UserCreationForm(request, data=request.POST or None)
+    form = UserCreationForm(data=request.POST or None)
     if form.is_valid():
-        print(form.cleaned_data)
         user = form.save(commit=True)
-        user.set_password(form.cleaned_data('password1'))
+        user.set_password(form.cleaned_data.get("password1"))
+        # send a confirmation email to verify their account
         login(request, user)
-        return redirect('/')
+        return redirect("/")
     context = {
         "form": form,
         "btn_label": "Register",
